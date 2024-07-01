10a396665e166f8bbf3e41e2f6eff12a2ccaf00d
sherryadan <adanmohammad80@gmail.com>
2024-07-02 01:54:43 +0500

    React Day 8

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Alert from "./components/Alert";
+// import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [gmode, setGmode] = useState("light");
+
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#131314";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+    }
+  };
+  const toggleGmode = () => {
+    if (gmode === "light") {
+      setGmode("green");
+      document.body.style.backgroundColor = "#5ab86d";
+      showAlert("Green mode has been enabled", "success");
+    } else {
+      setGmode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar
+        title="TextUtils"
+        aboutText="About TextUtils"
+        mode={mode}
+        toggleMode={toggleMode}
+        gmode={gmode}
+        toggleGmode={toggleGmode}
+      />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <TextForm
+          heading="Enter the text to analyze below"
+          buttonText="Convert to Uppercase"
+          button2Text="Convert to Lowercase"
+          button3Text="Clear All"
+          button4Text="Copy All"
+          mode={mode}
+          toggleMode={toggleMode}
+          showAlert={showAlert}
+          gmode={gmode}
+          toggleGmode={toggleGmode}
+        />
+        {/* <About /> */}
+      </div>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+// import PropTypes from "prop-types";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    console.log("Uppercase was clicked" + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to uppercase!", "success");
+  };
+
+  const handleLoClick = () => {
+    console.log("Uppercase was clicked" + text);
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to lowercase!", "success");
+  };
+  const handleClearClick = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text cleared!", "success");
+  };
+  const handleOnChange = (event) => {
+    console.log("On change");
+    setText(event.target.value);
+  };
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+    props.showAlert("Copied to clipboard!", "success");
+  };
+  const [text, setText] = useState("");
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <div className="mb-3">
+          <h1>{props.heading}</h1>
+          <textarea
+            className="form-control"
+            id="myBox"
+            value={text}
+            onChange={handleOnChange}
+            rows="10"
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
+          ></textarea>
+        </div>
+        <button className="btn btn-success mx-2" onClick={handleUpClick}>
+          {props.buttonText}
+        </button>
+        <button className="btn btn-success mx-2" onClick={handleLoClick}>
+          {props.button2Text}
+        </button>
+        <button className="btn btn-success mx-2" onClick={handleClearClick}>
+          {props.button3Text}
+        </button>
+        <button className="btn btn-success mx-2" onClick={handleCopy}>
+          {props.button4Text}
+        </button>
+      </div>
+      <div
+        className="container"
+        style={{
+          color: props.mode === "dark" ? "white" : "black",
+        }}
+      >
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>
+          {text.length > 0
+            ? text
+            : "Enter something in the textbox above to preview it here"}
+        </p>
+      </div>
+    </>
+  );
+}
