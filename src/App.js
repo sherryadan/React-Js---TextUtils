7024551d29e8068dbdc0501028d8bf241ae74b80
sherryadan <adanmohammad80@gmail.com>
2024-07-02 19:34:45 +0500

    React Day 9

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import Alert from "./components/Alert";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   const [mode, setMode] = useState("light");
   const [gmode, setGmode] = useState("light");
@@ -26,10 +26,12 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#131314";
       showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Home";
     }
   };
   const toggleGmode = () => {
@@ -37,39 +39,46 @@ function App() {
       setGmode("green");
       document.body.style.backgroundColor = "#5ab86d";
       showAlert("Green mode has been enabled", "success");
+      document.title = "TextUtils - Green Mode";
     } else {
       setGmode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Home";
     }
   };
 
   return (
     <>
-      <Navbar
-        title="TextUtils"
-        aboutText="About TextUtils"
-        mode={mode}
-        toggleMode={toggleMode}
-        gmode={gmode}
-        toggleGmode={toggleGmode}
-      />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm
-          heading="Enter the text to analyze below"
-          buttonText="Convert to Uppercase"
-          button2Text="Convert to Lowercase"
-          button3Text="Clear All"
-          button4Text="Copy All"
+      <Router>
+        <Navbar
+          title="TextUtils"
+          aboutText="About"
           mode={mode}
           toggleMode={toggleMode}
-          showAlert={showAlert}
           gmode={gmode}
           toggleGmode={toggleGmode}
         />
-        {/* <About /> */}
-      </div>
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About />} />
+
+            <Route
+              path="/"
+              element={
+                <TextForm
+                  heading="Enter the text to analyze below"
+                  buttonText="Convert to Uppercase"
+                  button2Text="Convert to Lowercase"
+                  button3Text="Clear All"
+                  button4Text="Copy All"
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
